871c5be6b87241cc3411ede55c693e0971667c83
Alemanuel <alemanuelj5@gmail.com>
2026-05-11 22:21:30 -0600

    songs

--- create_songs_table.php
+++ create_songs_table.php
@@ -1,16 +1,11 @@
-public function up(): void
-{
+public function up(): void {
     Schema::create('songs', function (Blueprint $table) {
         $table->id();
         $table->string('title');
         $table->integer('duration_seconds');
         $table->foreignId('album_id')->constrained()->onDelete('cascade');
-        $table->foreignId('genre_id')->constrained();
+        $table->foreignId('genre_id')->constrained()->onDelete('restrict');
         $table->timestamps();
     });
 }
-
-public function down(): void
-{
-    Schema::dropIfExists('songs');
-}
+public function down(): void { Schema::dropIfExists('songs'); }
